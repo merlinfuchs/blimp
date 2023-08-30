ba3c5c9045426f392d895d7f19a25bb4e0f0675c
merlinfuchs <merlinfuchs2001@gmail.com>
2023-08-30 07:54:04 +0200

    add notice about outdated config values

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/merlinfuchs/blimp/internal"
 	"github.com/merlinfuchs/blimp/internal/config"
+	"github.com/merlinfuchs/blimp/internal/logging"
 	"github.com/rs/zerolog/log"
 )
 
+var outDatedConfigKeys = []string{
+	"layout",
+	"views",
+}
+
 func main() {
 	config.InitConfig()
+	logging.InitLogger()
+
+	for _, key := range outDatedConfigKeys {
+		if config.K.Get(key) != nil {
+			fmt.Printf("Config key '%s' is no longer supported, please take a look at https://github.com/merlinfuchs/blimp#readme to see how to configure blimp!", key)
+			return
+		}
+	}
 
 	err := internal.AppEntry()
 	if err != nil {
